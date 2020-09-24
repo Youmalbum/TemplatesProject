a4c69e35c7bf1ae0f4e696b82ca4dc67764038ab
Javier <jaxier.rod@gmail.com>
2020-09-24 06:11:18 -0400

    IClientes

---
+++ NR.Core/Interfaz/IClientes.cs
@@ -0,0 +1,13 @@
+﻿using NR.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NR.Core.Interfaz
+{
+    public interface IClientes : IEntidadCrud<Clientes>
+    {
+        IEnumerable<SelectListItem> ListaSelect();
+    }
+}

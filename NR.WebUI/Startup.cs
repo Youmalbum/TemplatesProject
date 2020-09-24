cfe0487318dbe48ba8d619b46cea68c0e20b6401
Javier <jaxier.rod@gmail.com>
2020-09-24 06:20:04 -0400

    IClientes

--- NR.WebUI/Startup.cs
+++ NR.WebUI/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NR.Core.Interfaz;
+using NR.Core.Models;
+using NR.Core.Repository;
 
 namespace NR.WebUI
 {
@@ -24,6 +27,7 @@ namespace NR.WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IClientes, ClientesRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
